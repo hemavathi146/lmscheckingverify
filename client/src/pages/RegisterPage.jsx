fe98d69de74bf0f1c22ec1ba04aa4e7ffcbc8736
d3mmalition <ecarr.gardner@gmail.com>
2023-03-24 08:12:37 -0400

    updated routes for registration

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,16 +1,48 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 export default function RegisterPage() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function registerUser(ev) {
+    ev.preventDefault();
+    axios.get("/test");
+  }
+
   return (
     <div className="mt-4 grow flex items-center justify-around">
       <div className="mb-32">
         <h1 className="text-4xl text-center mb-4 ">Register</h1>
-        <form className="max-w-md mx-auto">
-          <input type="text" placeholder="First Name" />
-          <input type="text" placeholder="Last Name" />
-          <input type="email" placeholder="your@email.com" />
-          <input type="password" placeholder="password" />
-          <button className="primary">Login</button>
+        <form className="max-w-md mx-auto" onSubmit={registerUser}>
+          <input
+            type="text"
+            placeholder="First Name"
+            value={firstName}
+            onChange={(ev) => setFirstName(ev.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Last Name"
+            value={lastName}
+            onChange={(ev) => setLastName(ev.target.value)}
+          />
+          <input
+            type="email"
+            placeholder="your@email.com"
+            value={email}
+            onChange={(ev) => setEmail(ev.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="password"
+            value={password}
+            onChange={(ev) => setPassword(ev.target.value)}
+          />
+          <button className="primary">Register</button>
           <div className="text-center py-2 text-gray-500">
             Already have an account?{" "}
             <Link className="underline text-black" to={"/login"}>
