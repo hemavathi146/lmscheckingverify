df2f7fcb65279fe770e4001ac4c4734a6f379262
d3mmalition <ecarr.gardner@gmail.com>
2023-03-25 22:55:12 -0400

    name to accountpage

--- client/src/pages/AccountPage.jsx
+++ client/src/pages/AccountPage.jsx
@@ -1,3 +1,12 @@
+import { useContext } from "react";
+import { Navigate } from "react-router-dom";
+import { UserContext } from "../UserContext";
+
 export default function AccountPage() {
-  return <div>Account Page</div>;
+  const { user } = useContext(UserContext);
+
+  if (!user) {
+    return <Navigate to={"/login"} />;
+  }
+  return <div>Account Page for {user.firstName}</div>;
 }
