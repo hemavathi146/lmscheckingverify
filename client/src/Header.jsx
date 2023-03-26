561f51383bec5272ba7a86f9d7f9da1a683e2e52
d3mmalition <ecarr.gardner@gmail.com>
2023-03-25 22:51:16 -0400

    logo returns to /

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
   const { user } = useContext(UserContext);
   return (
     <header className="flex justify-between">
-      <a href="" className="flex items-center">
+      <Link to={"/"} className="flex items-center">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
@@ -22,7 +22,7 @@ export default function Header() {
           />
         </svg>
         <span className="font-bold text-xl">bookme</span>
-      </a>
+      </Link>
       <div className="flex gap-2 border border-gray-300 rounded-full py-2 px-4 shadow-md shadow-gray-300">
         <div>Anywhere</div>
         <div className="border border-l border-gray-300"></div>
