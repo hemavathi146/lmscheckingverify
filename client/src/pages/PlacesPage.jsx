ebb49e3d808b12c8b7cf3684683d404500185dde
d3mmalition <ecarr.gardner@gmail.com>
2023-03-27 08:00:38 -0400

    added icons

---
+++ client/src/pages/PlacesPage.jsx
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+
+export default function PlacesPage() {
+  return (
+    <div>
+      <div className="text-center ">
+        <Link
+          className="inline-flex gap-1 bg-primary text-white py-2 px-6 rounded-full"
+          to={"/account/places/new"}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 6v12m6-6H6"
+            />
+          </svg>
+          Add new place
+        </Link>
+      </div>
+      My places
+    </div>
+  );
+}
