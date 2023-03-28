979084fab8ceb84754090cad665ac998b55eb26d
d3mmalition <ecarr.gardner@gmail.com>
2023-03-28 15:27:49 -0400

    add place form

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -170,6 +170,46 @@ export default function PlacesPage() {
 
                 <span>TV & Cable</span>
               </label>
+              <label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
+                <input type="checkbox" />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M15.75 5.25a3 3 0 013 3m3 0a6 6 0 01-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1121.75 8.25z"
+                  />
+                </svg>
+                <span>24/7 Security</span>
+              </label>
+            </div>
+
+            <h2 className="text-2xl mt-4">Extra Info</h2>
+            <p className="text-gray-500">House rules, etc.</p>
+            <textarea />
+            <h2 className="text-2xl mt-4">Check in & out Times, Max Guests</h2>
+            <p className="text-gray-500">When can guests arrive? </p>
+            <div className="grid gap-2 sm:grid-cols-3">
+              <div>
+                <h3 className="mt-2 -mb-2">Check in Time</h3>
+                <input type="text" placeholder="14:00" />
+              </div>
+              <div>
+                <h3 className="mt-2 -mb-2">Check in Time</h3>
+
+                <input type="text" placeholder="" />
+              </div>
+              <div>
+                <h3 className="mt-2 -mb-2">Max Guests</h3>
+
+                <input type="text" />
+              </div>
             </div>
           </form>
         </div>
