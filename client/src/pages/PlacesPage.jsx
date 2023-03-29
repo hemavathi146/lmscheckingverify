b1d41183e266ed2ec5616b7711e933833b0ed6d9
d3mmalition <ecarr.gardner@gmail.com>
2023-03-28 20:44:36 -0400

    add photo function

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import Perks from "../Perks";
+import axios from "axios";
 
 export default function PlacesPage() {
   const { action } = useParams();
@@ -28,6 +29,11 @@ export default function PlacesPage() {
         {inputDescription(description)}
       </>
     );
+  }
+
+  async function addPhotoByLink(ev) {
+    ev.preventDefault();
+    const { data } = await axios.post("/upload-by-link", { link: photoLink });
   }
 
   return (
@@ -83,7 +89,10 @@ export default function PlacesPage() {
                 onChange={(ev) => setPhotoLink(ev.target.value)}
                 placeholder="Add using a link ... jpeg"
               />
-              <button className="bg-gray-200 px-4 rounded-2xl">
+              <button
+                onClick={addPhotoByLink}
+                className="bg-gray-200 px-4 rounded-2xl"
+              >
                 Add&nbsp;photo
               </button>
             </div>
