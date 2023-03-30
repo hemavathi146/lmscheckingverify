2ee2572556e62b30902d6fde2b0879c19de62832
d3mmalition <ecarr.gardner@gmail.com>
2023-03-29 21:55:43 -0400

    multer package

--- api/index.js
+++ api/index.js
@@ -7,6 +7,8 @@ const jwt = require('jsonwebtoken')
 const User = require('./models/user.js')
 const cookieParser = require('cookie-parser')
 const imageDownloader = require('image-downloader')
+const multer = require('multer');
+const fs= require('fs')
 
 const bcryptSalt = bcrypt.genSaltSync(10);
 const jwtSecret = 'adlfkasdfafoweirud'
@@ -87,11 +89,26 @@ app.post('/upload-by-link', async (req, res) => {
     const {link} = req.body
     const newName = 'photo' + Date.now() + '.jpg'
     await imageDownloader.image({
-        url:link,
-        dest: __dirname+'/uploads/' +newName
+        url: link,
+        dest: __dirname+'/uploads/' +newName,
     });
     res.json(newName) 
 
+})
+
+   const photosMiddleware = multer({dest:'uploads/'});
+app.post('/uploads', photosMiddleware.array('photos',100), (req, res) =>{
+    const uploadedFiles = [];
+    for (let i = 0; i<req.files.length; i++){
+        const {path, originalname} = req.files[i];
+        const parts = originalname.split('.')
+        const ext = parts[parts.length -1]
+        const newPath = path + '.' + ext
+        fs.renameSync(path, newPath)
+        uploadedFiles.push(newPath.replace('uploads/',''))
+
+    }
+    res.json(uploadedFiles)
 })
 
 app.listen(4000)
