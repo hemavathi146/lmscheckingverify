e7ba9e779b9c0366cf7ed4a671989e698c31cbda
d3mmalition <ecarr.gardner@gmail.com>
2023-04-02 18:30:34 -0400

    update places pg

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -38,9 +38,20 @@ export default function PlacesPage() {
       <div className="mt-4">
         {places.length > 0 &&
           places.map((place) => (
-            <div className="bg-gray-200 p-4 rounded-2xl" key={""}>
-              {place.title}
-            </div>
+            <Link
+              to={"/account/places/" + places._id}
+              className="flex cursor-pointer gap-4 bg-gray-100 p-4 rounded-2xl"
+            >
+              <div className="w-32 h-32 bg-gray-300 grow shrink-0">
+                {place.photos.length > 0 && (
+                  <img src={place.photos[0]} alt="" />
+                )}
+              </div>
+              <div className="grow-0 shrink">
+                <h2 className="text-xl">{place.title}</h2>
+                <p className="">{place.description}</p>
+              </div>
+            </Link>
           ))}
       </div>
     </div>
