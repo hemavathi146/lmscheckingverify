0856b1b92c232c230246b51a32251fe6af392cc2
d3mmalition <ecarr.gardner@gmail.com>
2023-04-03 20:00:07 -0400

    editable accomodations

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -39,7 +39,7 @@ export default function PlacesPage() {
         {places.length > 0 &&
           places.map((place) => (
             <Link
-              to={"/account/places/" + places._id}
+              to={"/account/places/" + place._id}
               className="flex cursor-pointer gap-4 bg-gray-100 p-4 rounded-2xl"
             >
               <div className="w-32 h-32 bg-gray-300 grow shrink-0">
@@ -49,7 +49,7 @@ export default function PlacesPage() {
               </div>
               <div className="grow-0 shrink">
                 <h2 className="text-xl">{place.title}</h2>
-                <p className="">{place.description}</p>
+                <p className="text-sm mt-2 ">{place.description}</p>
               </div>
             </Link>
           ))}
