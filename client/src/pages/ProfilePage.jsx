bbb31895f7dc57407e0308050ee076a9bf53ca51
d3mmalition <ecarr.gardner@gmail.com>
2023-04-04 21:34:41 -0400

    refactor

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,6 +1,6 @@
 import { useContext, useState } from "react";
+import { UserContext } from "../UserContext.jsx";
 import { Link, Navigate, useParams } from "react-router-dom";
-import { UserContext } from "../UserContext";
 import axios from "axios";
 import PlacesPage from "./PlacesPage";
 import AccountNav from "../AccountNav";
@@ -8,7 +8,6 @@ import AccountNav from "../AccountNav";
 export default function ProfilePage() {
   const [redirect, setRedirect] = useState(null);
   const { ready, user, setUser } = useContext(UserContext);
-
   let { subpage } = useParams();
   if (subpage === undefined) {
     subpage = "profile";
@@ -31,13 +30,12 @@ export default function ProfilePage() {
   if (redirect) {
     return <Navigate to={redirect} />;
   }
-
   return (
     <div>
       <AccountNav />
       {subpage === "profile" && (
-        <div className="text-center max-w-xl mx-auto">
-          Logged in as {user.firstName} {user.lastName} ({user.email})
+        <div className="text-center max-w-lg mx-auto">
+          Logged in as {user.name} ({user.email})<br />
           <button onClick={logout} className="primary max-w-sm mt-2">
             Logout
           </button>
