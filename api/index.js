17a791d3d7eceffabf0f1ebcb819a0f20f562bf1
d3mmalition <ecarr.gardner@gmail.com>
2023-04-04 22:08:34 -0400

    build prep

--- api/index.js
+++ api/index.js
@@ -1,31 +1,31 @@
-const express = require("express");
-const cors = require ("cors")
-const mongoose = require("mongoose")
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
-const User = require('./models/User.js')
-const Place = require('./models/Place.js')
+const express = require('express');
+const cors = require('cors');
+const mongoose = require("mongoose");
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('./models/User.js');
+const Place = require('./models/Place.js');
 const Booking = require('./models/Booking.js');
-const cookieParser = require('cookie-parser')
-const imageDownloader = require('image-downloader')
+const cookieParser = require('cookie-parser');
+const imageDownloader = require('image-downloader');
 const {S3Client, PutObjectCommand} = require('@aws-sdk/client-s3');
 const multer = require('multer');
-const fs= require('fs')
+const fs = require('fs');
 const mime = require('mime-types');
 
-require('dotenv').config()
+require('dotenv').config();
 const app = express();
 
 const bcryptSalt = bcrypt.genSaltSync(10);
-const jwtSecret = 'adlfkasdfafoweirud'
+const jwtSecret = 'fasefraw4r5r3wq45wdfgw34twdfg';
 const bucket = 'bookme-clone-app';
 
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 app.use('/uploads', express.static(__dirname+'/uploads'));
 app.use(cors({
-    credentials: true,
-    origin: 'http://localhost:5173',
+  credentials: true,
+  origin: 'http://localhost:5173',
 }));
 
 async function uploadToS3(path, originalFilename, mimetype) {
@@ -49,80 +49,83 @@ async function uploadToS3(path, originalFilename, mimetype) {
   return `https://${bucket}.s3.amazonaws.com/${newFilename}`;
 }
 
-// function getUserDataFromReq(req) {
-//   return new Promise((resolve, reject) => {
-//     jwt.verify(req.cookies.token, jwtSecret, {}, async (err, userData) => {
-//       if (err) throw err;
-//       resolve(userData);
-//     });
-//   });
-// }
-
-app.get('/api/test', (req, res) => {
-    mongoose.connect(process.env.MONGO_URL);
-    res.json('test ok')
-})
-
-app.post('/api/register', async (req, res) =>{
-        mongoose.connect(process.env.MONGO_URL);
-    const {firstName, lastName, email, password} = req.body;
-
-try {
-    const userDoc = await User.create({
-        firstName,
-        lastName,
-        email,
-        password:bcrypt.hashSync(password, bcryptSalt),
-    })
-    res.json(userDoc); 
-} catch(e) {
-    res.status(422).json(e)
+function getUserDataFromReq(req) {
+  return new Promise((resolve, reject) => {
+    jwt.verify(req.cookies.token, jwtSecret, {}, async (err, userData) => {
+      if (err) throw err;
+      resolve(userData);
+    });
+  });
 }
-})
 
-app.post("/api/login", async (req, res) => {
-            mongoose.connect(process.env.MONGO_URL);
-    const {email, password} = req.body
-    const userDoc = await User.findOne({email})
-    if (userDoc) {
-        const passOk = bcrypt.compareSync(password, userDoc.password)
-        if (passOk) {
-            jwt.sign({email:userDoc.email, id:userDoc._id}, jwtSecret, {} , (err,token) => {
-                if(err) throw err; 
-                res.cookie('token', token).json(userDoc)
-            })
-            
-        } else {
-            res.status(422).json('pass not ok')
-        }
+app.get('/api/test', (req,res) => {
+  mongoose.connect(process.env.MONGO_URL);
+  res.json('test ok');
+});
+
+app.post('/api/register', async (req,res) => {
+  mongoose.connect(process.env.MONGO_URL);
+  const {name,email,password} = req.body;
+
+  try {
+    const userDoc = await User.create({
+      name,
+      email,
+      password:bcrypt.hashSync(password, bcryptSalt),
+    });
+    res.json(userDoc);
+  } catch (e) {
+    res.status(422).json(e);
+  }
+
+});
+
+app.post('/api/login', async (req,res) => {
+  mongoose.connect(process.env.MONGO_URL);
+  const {email,password} = req.body;
+  const userDoc = await User.findOne({email});
+  if (userDoc) {
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    if (passOk) {
+      jwt.sign({
+        email:userDoc.email,
+        id:userDoc._id
+      }, jwtSecret, {}, (err,token) => {
+        if (err) throw err;
+        res.cookie('token', token).json(userDoc);
+      });
     } else {
-        res.status(422).json('not found')
+      res.status(422).json('pass not ok');
     }
-})
+  } else {
+    res.json('not found');
+  }
+});
 
-app.get('/api/profile', (req, res) => {
+app.get('/api/profile', (req,res) => {
+  mongoose.connect(process.env.MONGO_URL);
+  const {token} = req.cookies;
+  if (token) {
+    jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+      if (err) throw err;
+      const {name,email,_id} = await User.findById(userData.id);
+      res.json({name,email,_id});
+    });
+  } else {
+    res.json(null);
+  }
+});
+
+app.post('/api/logout', (req,res) => {
+  res.cookie('token', '').json(true);
+});
+
+
+app.post('/api/upload-by-link', async (req,res) => {
     mongoose.connect(process.env.MONGO_URL);
-    const {token} = req.cookies;
-    if (token) {
-        jwt.verify(token, jwtSecret, {}, async (err, userData) => {
-            if(err) throw err;
-            const {firstName, lastName, email, _id} = 
-            await User.findById(userData.id)
-            res.json({firstName, lastName, email, _id}) 
-        })
-    } else {
-        res.json(null)
-    }
-});
-
-app.post("/api/logout", (req, res) => {
-    res.cookie("token", '').json(true);
-});
-
-app.post('/api/upload-by-link', async (req, res) => {
-    const {link} = req.body;
-    const newName = 'photo' + Date.now() + '.jpg';
-    await imageDownloader.image({
+  const {link} = req.body;
+  const newName = 'photo' + Date.now() + '.jpg';
+  await imageDownloader.image({
     url: link,
     dest: '/tmp/' +newName,
   });
@@ -224,5 +227,4 @@ app.get('/api/bookings', async (req,res) => {
   res.json( await Booking.find({user:userData.id}).populate('place') );
 });
 
-
-app.listen(4000)
+app.listen(4000);
