b595743287342caf0ecdc7ee12689a2ba649f950
d3mmalition <ecarr.gardner@gmail.com>
2023-04-04 21:31:24 -0400

    routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,8 +9,11 @@ import axios from "axios";
 import { UserContextProvider } from "./UserContext";
 import PlacesPage from "./pages/PlacesPage";
 import PlacesFormPage from "./pages/PlacesFormPage";
+import BookingsPage from "./pages/BookingsPage";
+import BookingPage from "./pages/BookingPage";
+import PlacePage from "./pages/PlacesPage";
 
-axios.defaults.baseURL = "http://localhost:4000";
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
 axios.defaults.withCredentials = true;
 
 function App() {
@@ -25,6 +28,9 @@ function App() {
           <Route path="/account/places" element={<PlacesPage />} />
           <Route path="/account/places/new" element={<PlacesFormPage />} />
           <Route path="/account/places/:id" element={<PlacesFormPage />} />
+          <Route path="/place/:id" element={<PlacePage />} />
+          <Route path="/account/bookings" element={<BookingsPage />} />
+          <Route path="/account/bookings/:id" element={<BookingPage />} />
         </Route>
       </Routes>
     </UserContextProvider>
