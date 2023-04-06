218b798005b06cceb45fdb7b500ea907b13a0ec0
d3mmalition <ecarr.gardner@gmail.com>
2023-04-06 19:10:38 -0400

    add for nonauthenticated

--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -4,14 +4,16 @@ import { Link } from "react-router-dom";
 
 export default function IndexPage() {
   const [places, setPlaces] = useState([]);
+
   useEffect(() => {
     axios.get("/places").then((response) => {
       setPlaces(response.data);
     });
   }, []);
+
   return (
     <div className="mt-8 grid gap-x-6 gap-y-8 grid-cols-2 md:grid-cols-3 lg:grid-cols-3">
-      {places.length > 0 &&
+      {places.length > 0 ? (
         places.map((place) => (
           <Link to={"/place/" + place._id}>
             <div className="bg-gray-500 mb-2 rounded-2xl flex">
@@ -29,7 +31,10 @@ export default function IndexPage() {
               <span className="font-bold">${place.price}</span> per night
             </div>
           </Link>
-        ))}
+        ))
+      ) : (
+        <p>No accommodations found.</p>
+      )}
     </div>
   );
 }
