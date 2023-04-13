e7adc92c1d987926aada996f769de38558b025a8
d3mmalition <ecarr.gardner@gmail.com>
2023-04-12 22:26:37 -0400

    origin: true

--- api/index.js
+++ api/index.js
@@ -25,7 +25,7 @@ app.use(cookieParser())
 app.use('/uploads', express.static(__dirname+'/uploads'))
 app.use(cors({
     credentials: true,
-    origin: ['http://localhost:5173', 'https://booking-clone-64trt2r9d-d3mmalition.vercel.app/'],
+    origin: true,
 }));
 
 async function uploadToS3(path, originalFilename, mimetype) {
