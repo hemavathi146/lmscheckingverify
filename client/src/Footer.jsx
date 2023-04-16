a4ef8c532c1cbff5de44d65fa4d0445c487f6ebb
d3mmalition <ecarr.gardner@gmail.com>
2023-04-15 20:38:34 -0400

    update footer

--- client/src/Footer.jsx
+++ client/src/Footer.jsx
@@ -6,15 +6,17 @@ export default function () {
       <hr />
       <div className="md:flex md:justify-between md:items-center sm:px-12 px-4 bg-white py-7">
         <div>
-          <h3 className="">Contact</h3>
+          <h3 className="">Contact Us</h3>
           <hr />
           <p className="text-sm">ecarr.gardner@gmail.com</p>
         </div>
         <div>
           <h3>Connect With Us</h3>
           <a href="">Image 1</a>
+          <a href="">Image 2</a>
+          <a href="">Image 3</a>
         </div>
-        <div>Visit</div>
+        <div>Visit Us</div>
       </div>
     </footer>
   );
