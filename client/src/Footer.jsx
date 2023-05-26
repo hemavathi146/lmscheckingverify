5a3f02470dd9a2d1e8fd8d33baa566eea8559ebe
d3mmalition <ecarr.gardner@gmail.com>
2023-05-25 20:29:19 -0400

    update footer

--- client/src/Footer.jsx
+++ client/src/Footer.jsx
@@ -3,20 +3,14 @@ import React from "react";
 export default function () {
   return (
     <footer className="">
-      <hr />
       <div className="md:flex md:justify-between md:items-center sm:px-12 px-4 bg-white py-7">
         <div>
-          <h3 className="">Contact Us</h3>
-          <hr />
-          <p className="text-sm">ecarr.gardner@gmail.com</p>
-        </div>
-        <div>
           <h3>Connect With Us</h3>
-          <a href="">Icon</a>
-          <a href="">Icon 2</a>
+          <hr />
+          <a href="https://linkedin.com/in/ecarrgardner">fab fa-linkedin</a>
+          <a href="https://github.com/d3mmalition">fab fa-github</a>
           <a href="">Icon 3</a>
         </div>
-        <div>Third Row</div>
       </div>
     </footer>
   );
